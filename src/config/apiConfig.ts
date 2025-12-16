322367bc5a34bde9e4192f83696b8b5bb64759da
Prajyot Sancheti <prajyotsancheti1772gmail.com>
2025-12-16 11:09:27 +0530

    chagnes

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    baseUrl: import.meta.env.VITE_API_URL || 'https://auction.vardhamanpaper.com',
 };
 
 export default apiConfig;
